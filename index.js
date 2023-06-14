2a20d8c262da5d79b73172723b0b155b68a83c50
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-14 08:42:08 +0300

    Done with assignments

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => {
+    const results = 2000 / 100;
+    return results;
+}
+const square = (mult) => {
+    const answer = mult * mult;
+    return answer;
+
+}
+const add = (addition1,addition2 ) => {
+    const adding = addition1 + addition2;
+    return adding;
+
+}
